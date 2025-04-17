01ff1de0d2fce3f3c7a924d39ffc2ee33b673a2f
mattchristenson <matt@rcc.team>
2025-04-17 14:02:06 -0500

    finish ex 1,2

--- FullNameParser/src/main/java/com/pluralsight/Main.java
+++ FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,14 +9,47 @@ public class Main {
 
         // establish known values
         System.out.print("What is your full name?: ");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine().trim();
 
         //calculate the unknown
 
-        
-        String firstName = "";
-        String middleName = "";
-        String lastName = "";
+        //int firstSpaceIndex = name.indexOf(" ");
+
+
+        //String firstName = name.substring(0, firstSpaceIndex);
+
+        //int secondSpaceIndex = name.indexOf(" ", firstSpaceIndex + 1);
+
+        //String middleName;
+        //String lastName;
+
+//        if(secondSpaceIndex >= 0){
+//            //we have middle and last name
+//            middleName = name.substring(firstSpaceIndex + 1, secondSpaceIndex);
+//            lastName = name.substring(secondSpaceIndex + 1);
+//        }
+//        else{
+//            //we have only last name
+//            middleName = "(none)";
+//            lastName = name.substring(firstSpaceIndex + 1);
+//        }
+
+        String[] names = name.split(Pattern.quote(" "));
+
+        String firstName = names[0];
+        String middleName;
+        String lastName;
+
+        if(names.length == 2){
+            middleName = "(none)";
+           // lastName = names[1];
+        }
+        else{
+            middleName = names[1];
+            //lastName = names[2];
+        }
+
+        lastName = names[names.length -1 ];
 
         //display the results
         System.out.println();
