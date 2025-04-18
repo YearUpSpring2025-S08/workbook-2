8b393cc1fa5d201f6686163d56930adb842c41d0
mattchristenson <matt@rcc.team>
2025-04-18 12:04:31 -0500

    consume the class

--- ClassDemo1/src/main/java/com/pluralsight/Main.java
+++ ClassDemo1/src/main/java/com/pluralsight/Main.java
@@ -6,24 +6,49 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String fullname;
+        int age;
+        String profession;
+
         System.out.print("What is the person's full name: ");
-        String fullname = scanner.nextLine();
+        fullname = scanner.nextLine();
 
         System.out.print("What is the person's age: ");
-        int age = scanner.nextInt();
+        age = scanner.nextInt();
         scanner.nextLine(); //clear the CRLF
 
         System.out.print("What is the person's profession: ");
-        String profession = scanner.nextLine();
+        profession = scanner.nextLine();
 
-        displayPerson(fullname, age, profession);
-        savePerson(fullname, age, profession);
+        Person thePerson = new Person(fullname, age, profession);
 
+        System.out.print("What is the person's full name: ");
+        fullname = scanner.nextLine();
 
+        System.out.print("What is the person's age: ");
+        age = scanner.nextInt();
+        scanner.nextLine(); //clear the CRLF
+
+        System.out.print("What is the person's profession: ");
+        profession = scanner.nextLine();
+
+        Person theOtherPerson = new Person(fullname, age, profession);
+
+        System.out.println(thePerson.getFullname());
+        System.out.println(theOtherPerson.getFullname());
+
+       displayPerson(thePerson);
+       displayPerson(theOtherPerson);
+
+       // savePerson(fullname, age, profession);
     }
 
-    public static void displayPerson(String fullname, int age, String profession){
-        System.out.printf("Person %s is a %s and is %d years old.\n", fullname, profession, age);
+    public static void displayPerson(Person person) {
+
+        System.out.printf("Person %s is a %s and is %d years old.\n"
+                , person.getFullname()
+                , person.getProfession()
+                , person.getAge());
     }
 
     public static void savePerson(String fullname, int age, String profession){
